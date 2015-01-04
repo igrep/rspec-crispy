e01eb2aae6d2dcbec25f13f2bcea16446e12e187
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-01-04 14:18:40 +0900

    outline for configure_without_conflict_spec

--- spec/rspec/crispy/configure_without_conflict_spec.rb
+++ spec/rspec/crispy/configure_without_conflict_spec.rb
@@ -4,3 +4,35 @@ RSpec.configure do|config|
   ::RSpec::Crispy.configure_without_conflict config
 end
 
+RSpec.describe ::RSpec::Crispy do
+
+  context 'when including' do
+    include ::RSpec::Crispy
+
+    describe '#spy' do
+    end
+
+    describe '#double' do
+    end
+
+    describe '#have_received' do
+    end
+
+    describe '#have_received_once' do
+    end
+  end
+
+  context 'when not including' do
+
+    describe '#spy' do
+    end
+
+    describe '#double' do
+    end
+
+    describe '#have_received' do
+    end
+
+  end
+
+end
